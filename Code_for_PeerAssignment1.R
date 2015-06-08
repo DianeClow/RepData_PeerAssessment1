4410fb96d5e9a61aff48c3c657b5ac396d1fde0a
DianeClow <diane@clow.org>
2015-06-07 22:08:33 -0700

    Added Code
    I was able to find a way to replace the missing NA values with the
    average interval value.

--- Code_for_PeerAssignment1.R
+++ Code_for_PeerAssignment1.R
@@ -9,5 +9,18 @@ time_interval_steps <- aggregate(steps ~ interval, activity, mean)
 plot(time_interval_steps$steps ~ time_interval_steps$interval, 
      type="l", main = "Average Number of Steps per Interval", 
      ylab="Number of Steps", xlab="Interval (5 minutes)")
-
 max <- max(time_interval_steps$steps)
+Max_Interval <- subset(time_interval_steps, time_interval_steps$steps == max)
+
+### Part 3
+NA_Values <- sum(is.na(activity$steps))
+activity2 <- activity ## copies the data set so that the original one is not modified
+len <- nrow(activity2) ## calculates how many rows in the data set
+for(i in 1:len){
+    if(is.na(activity2$steps[i]) == TRUE){ ##checks to see if the value is missing
+        int <- activity2$interval[i] ## caluclates the interval number
+        temp <- subset(time_interval_steps, interval == int) ## creates a temporary data frame that has been subseted to the single interval value needed
+        activity2$steps[i] <- temp$steps ## replaces the NA value with the average
+    }
+}
+
